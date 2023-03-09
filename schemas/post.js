7ff96faff52759e7cbb233f8ad149c7d2646ce7f
glmsr12 <gulumsercakmak@hotmail.com>
2023-03-08 16:20:13 -0800

    "update file"

--- schemas/post.js
+++ schemas/post.js
@@ -8,5 +8,24 @@ export default {
       type: 'geopoint',
       title: 'Location',
     },
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'author',
+      type: 'referance',
+      title: 'Author',
+    },
   ],
 }
