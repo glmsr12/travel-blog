a7b25c11f0369ae907f04f10d4fc92b1677d0f0c
glmsr12 <gulumsercakmak@hotmail.com>
2023-03-13 14:48:11 -0700

    "cateory and author schema implemented"

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,6 @@
 import post from './post'
 import blockContent from './blockContent'
+import category from './category'
+import author from './author'
 
-export const schemaTypes = [post, blockContent]
+export const schemaTypes = [post, blockContent, category, author]
--- schemas/post.js
+++ schemas/post.js
@@ -22,12 +22,12 @@ export default {
         maxLength: 96,
       },
     },
-    // {
-    //   name: 'author',
-    //   type: 'reference',
-    //   title: 'Author',
-    //   to: {type: 'author'},
-    // },
+    {
+      name: 'author',
+      type: 'reference',
+      title: 'Author',
+      to: {type: 'author'},
+    },
     {
       name: 'mainImage',
       type: 'image',
@@ -36,12 +36,12 @@ export default {
         hotspot: true,
       },
     },
-    // {
-    //   name: 'categories',
-    //   type: 'array',
-    //   title: 'Categories',
-    //   of: [{type: 'reference', to: {type: 'category'}}],
-    // },
+    {
+      name: 'categories',
+      type: 'array',
+      title: 'Categories',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    },
     {
       name: 'publishedAt',
       type: 'datetime',
